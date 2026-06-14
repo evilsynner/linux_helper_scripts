1f032fe69d3c5137a13291c4486036707050ba3e
Juan Lozano <168985262+evilsynner@users.noreply.github.com>
2026-06-14 11:47:03 -0500

    Validation checks have been added for the provided arguments; these checks will ensure that only one argument (a JSON file) is provided.

--- backup/backup_config.sh
+++ backup/backup_config.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-main() {
-    echo "Backup your configuration files."
+# COLOR SEQUENCES
+# \e[COLORmSample Text\e[0m
+
+read_json_file() {
+    jq ".backup_folder" $1
 }
 
-main
+
+echo "$@"
+
+if [ "$#" -gt 1 ]; then
+    echo -e "\e[1;31m[!] You must specify only the JSON file that contains the files and folders to be backed up.\e[0m"
+    exit 1
+elif [ "$#" -lt 1 ]; then
+    echo -e "\e[1;31m[!] You must specify the JSON file that contains the files and folders to be backed up.\e[0m"
+    exit 1
+fi
+
+read_json_file "$1"
